bf8786475a6f1d5128f4cfbc42d14ec49fcf27ff
Gareth Flowers <garethflowers@users.noreply.github.com>
2011-11-30 20:41:32 +0000

    Added 'Proccessing' classes

---
+++ src/garethflowers/retagit/ProcessReader.java
@@ -0,0 +1,48 @@
+package garethflowers.retagit;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * ProcessReader
+ * 
+ * @author Gareth Flowers <gareth@garethflowers.com>
+ */
+public class ProcessReader extends Thread {
+
+	private InputStream is;
+	private Collection<String> result;
+
+	public ProcessReader(InputStream is) {
+		this.is = is;
+		this.result = new ArrayList<String>();
+	}
+
+	@Override
+	public void run() {
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		String line = null;
+
+		try {
+			while ((line = br.readLine()) != null) {
+				result.add(line);
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getStackTrace());
+		}
+	}
+
+	public String[] GetResult() {
+		try {
+			return result.toArray(new String[0]);
+		} catch (Exception ex) {
+			System.out.println(ex.getStackTrace());
+			return new String[0];
+		}
+	}
+}
